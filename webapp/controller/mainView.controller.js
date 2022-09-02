4195d47b0f3c450181487221d861946d4e3f8f59
AnupamSAP <alok750anu@gmail.com>
2022-09-02 14:10:23 +0000

    check merge

--- webapp/controller/mainView.controller.js
+++ webapp/controller/mainView.controller.js
@@ -9,7 +9,7 @@ sap.ui.define([
 
         return Controller.extend("com.emp.empproject9.controller.mainView", {
             onInit: function () {
-             // I am second Developer                    
+             // I am second Developer but being 1st dev, i want mine code too
             },
             onTypeChar: function(oEvt){
                 //This is 1st developer code
